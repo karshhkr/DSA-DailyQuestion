af6831e73b99429492921d473ba7aec35c0896cd
Utkarsh Kumar Dabgarwal <utkarshkumardabgarwal@gmail.com>
2026-04-20 23:18:56 +0530

    Initial commit - Fresh Start

---
+++ src/main/java/com/dsa/TwoPointers/Solutsion.java
@@ -0,0 +1,10 @@
+package TwoPointers;
+
+import java.util.*;
+
+class Solutions{
+    public static void main(String[] args) {
+        int nums[]= {2,4,5,6,7}
+        for(int i =0; i<=n)
+    }
+}
