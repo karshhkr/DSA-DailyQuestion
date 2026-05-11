3bafd2f971f021d42af955ce1f6259bda7132d65
Utkarsh Kumar Dabgarwal <114185055+karshhkr@users.noreply.github.com>
2026-05-11 22:23:15 +0530

    Time: 0 ms (100%), Space: 46.2 MB (69.9%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -0,0 +1,27 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+    
+        {
+
+        int start=0;
+        int end =letters.length-1;
+        while(start<=end){
+            // finding the middle
+//        int mid=(start+end)/2; // might be possible that (start+mid)exceeds the range of int in  Java integer Overflow
+          
+            int mid=start+(end-start)/2;
+            if(target<letters[mid]){
+                end=mid-1;
+
+            }
+            else  {
+                start=mid+1;
+
+            }
+
+        }
+        return letters[start%letters.length];
+    }
+    
+    }
+}
