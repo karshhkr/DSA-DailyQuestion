9cbaf602113e00e0de0d31abaccd2a5743d69497
Utkarsh Kumar Dabgarwal <114185055+karshhkr@users.noreply.github.com>
2026-09-21 23:22:51 +0530

    Time: 1 ms (99.51%), Space: 47.1 MB (23.87%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,36 +1,68 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int [] dest = new int [arr.length];
-        int src =0;
-        int des=0;
+    //     int [] dest = new int [arr.length];
+    //     int src =0;
+    //     int des=0;
         
-        while(src<arr.length){
+    //     while(src<arr.length){
             
-            if(arr[src]==0){
-            if(des<arr.length){
-           dest[des]=0;
-            }
+    //         if(arr[src]==0){
+    //         if(des<arr.length){
+    //        dest[des]=0;
+    //         }
               
-                des+=1;
-                if(des<arr.length){
-                  dest[des]=0;
-                }
+    //             des+=1;
+    //             if(des<arr.length){
+    //               dest[des]=0;
+    //             }
               
 
-            }
-            else {
-                if(des<arr.length){
-                   dest[des]=arr[src];
-                }
+    //         }
+    //         else {
+    //             if(des<arr.length){
+    //                dest[des]=arr[src];
+    //             }
               
-            }
-             des+=1;
-        src+=1;
-        }
+    //         }
+    //          des+=1;
+    //     src+=1;
+    //     }
        
     
-    for (int i =0; i<arr.length; i++){
-        arr[i]= dest[i];
+    // for (int i =0; i<arr.length; i++){
+    //     arr[i]= dest[i];
+    // }
+
+
+
+
+    //approach 2:- In line
+    
+    int possibleZeroDupli=0;
+    int lastIndex=arr.length-1; 
+    for (int i =0; i<=lastIndex -possibleZeroDupli; i++){
+    if(arr[i]==0){
+         if(i==lastIndex - possibleZeroDupli){
+    
+     arr[lastIndex]=0;
+     lastIndex -=1;
+break;
+
     }
+     possibleZeroDupli++;
 }
+    }
+     int newLastIndex=lastIndex-possibleZeroDupli;
+      for (int i= newLastIndex; i>=0; i--){
+        
+        if(arr[i]==0){
+            arr[i+possibleZeroDupli]=0;
+            possibleZeroDupli--;
+            arr[i+possibleZeroDupli]=0;
+        }
+        else{
+            arr[i+possibleZeroDupli]=arr[i];
+        }
+      } 
+    }
 }
